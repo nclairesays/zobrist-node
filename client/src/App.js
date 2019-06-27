4f30bf4675dc06c287609f00a1878a05601dca9a
nclairesays <45305029+nclairesays@users.noreply.github.com>
2019-06-26 17:16:30 -0700

    added query logic

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,14 @@ state = {
   };
 
   componentDidMount() {
-    this.callBackendAPI()
-      .then(res => this.setState({ products: res.dataSource.products }))
+    this.callBackendAPI(1,8)
+      .then(res => this.setState({ products: res.products }))
       .catch(err => console.log(err));
   }
 
-  callBackendAPI = async () => {
-    const response = await fetch('/api/products');
+  callBackendAPI = async (n, m) => {
+    const query = (n || m) ? `?page=${n}&size=${m}` : ""
+    const response = await fetch(`/api/products${query}`);
     const body = await response.json();
     if (response.status !== 200) {
       throw Error(body.message) 
@@ -27,7 +28,7 @@ state = {
       <div className="app">
         <div className="all-products">
           {this.state.products.map ( product => {
-            return <Product {...product} />
+            return <Product key={product.title} {...product} />
           })}
          </div>
       </div>
--- server.js
+++ server.js
@@ -2,14 +2,29 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const dataSource = require('./dataSource.json')
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
 
 app.get('/api/products', (req, res) => {
-    res.send(
-        { dataSource: dataSource })
+    const {page, size} = req.query
+    const {products} = dataSource
+
+    if (page || size ) {
+        res.send({ products:queried(products, parseInt(page), parseInt(size)) })
+        
+    } else  {
+        console.log('SEND ALL')
+        res.send({ products })
+        
+    }
 
 });
 
+const queried = (arr, page, size) => {
+    let start = ( page - 1 ) * size
+    return arr.slice(start, start+size)
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
